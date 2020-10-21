627035afce9e53e5f0bbe0e580538be86f54c58c
Elad Mor <73258461+elad-mor@users.noreply.github.com>
2020-10-22 01:01:17 +0300

    Update ex1.cc

--- ex1.cc
+++ ex1.cc
@@ -10,8 +10,10 @@ int main()
   int side1, side 2, side 3, s;
   
   cin << side1 << side2 << side 3;
-  s = side1 + side2 + side3;
-  cout 
+  s = (side1 + side2 + side3) / 2;
+  cout >> side1 + side2 + side3;
+  cout >> sqrt(s * (s - side1) * (s - side2) * (s - side3));
+  
   
   return 0;
 }

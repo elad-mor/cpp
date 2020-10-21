904d600df4f99b5c12b48293bbaa4bbfe16856eb
Elad Mor <73258461+elad-mor@users.noreply.github.com>
2020-10-22 00:58:04 +0300

    Create ex1.cc

---
+++ ex1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std:cin;
+using std::endl;
+
+int main()
+{
+  int side1, side 2, side 3, s;
+  
+  cin << side1 << side2 << side 3;
+  s = side1 + side2 + side3;
+  cout 
+  
+  return 0;
+}

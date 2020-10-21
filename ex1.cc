2f1728e00f99c5404f912e638d95a18a7c5d1136
Elad Mor <73258461+elad-mor@users.noreply.github.com>
2020-10-22 01:03:28 +0300

    Update ex1.cc

--- ex1.cc
+++ ex1.cc
@@ -7,13 +7,12 @@ using std::endl;
 
 int main()
 {
-  int side1, side 2, side 3, s;
+  double side1, side2, side3, s; //declaring 3 doubles for each side of a triangle and one double to except the perimeter of the triangle.
   
-  cin << side1 << side2 << side 3;
+  cin << side1 << side2 << side 3; //add input for each side of the triangle.
   s = (side1 + side2 + side3) / 2;
   cout >> side1 + side2 + side3;
   cout >> sqrt(s * (s - side1) * (s - side2) * (s - side3));
-  
   
   return 0;
 }
